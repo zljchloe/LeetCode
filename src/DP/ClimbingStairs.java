6b5a21bf2c19c14d65eafd9de20347059cebc227
Lyujia Zhang <zljchloe@gmail.com>
2016-09-30 16:09:39 -0400

    DP & Matrix

--- src/DP/ClimbingStairs.java
+++
@@ -1,7 +0,0 @@
-package DP;
-
-/**
- * Created by lyujiazhang on 9/14/16.
- */
-public class ClimbingStairs {
-}
---
+++ src/DP/UniquePaths.java
@@ -0,0 +1,32 @@
+package DP;
+
+/**
+ * Created by lyujiazhang on 9/30/16.
+ * https://leetcode.com/problems/unique-paths/
+ *
+ * A robot is located at the top-left corner of a m x n grid.
+ * The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+ * How many possible unique paths are there?
+ *
+ * 1. M[i][j] represents the number of paths possible from original point to current position [i][j].
+ * 2. Base case: M[0][0] = 1, M[0][i] = 1 (i = 1...n - 1), M[i][0] = 1 (i = 1...m - 1).
+ * 3. Induction rule: M[i][j[ = M[i - 1][j] + M[i][j - 1].
+ */
+public class UniquePaths {
+    public int uniquePaths(int m, int n) {
+        int[][] matrix = new int[m][n];
+        matrix[0][0] = 1;
+        for (int i = 1; i < m; i++) {
+            matrix[i][0] = 1;
+        }
+        for (int i = 1; i < n; i++) {
+            matrix[0][i] = 1;
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                matrix[i][j] = matrix[i - 1][j] + matrix[i][j - 1];
+            }
+        }
+        return matrix[m - 1][n - 1];
+    }
+}
